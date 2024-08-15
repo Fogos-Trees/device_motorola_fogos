03577ec3b5bdce1da5437bd10f29254ae5d44cfd
drpto <dr.juniornoob@gmail.com>
2024-08-15 15:11:22 +0530

    fogos: Add vendorsetup file for easy cloneing
    Signed-off-by: drpto <dr.juniornoob@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/Fogos-Trees/device_xiaomi_sm6375-common.git device/motorola/sm6375-common
+git clone https://github.com/Fogos-Trees/vendor_motorola_fogos.git vendor/motorola/fogos
+git clone https://github.com/Fogos-Trees/vendor_motorola_sm6375-common.git vendor/motorola/sm6375-common
+git clone https://github.com/Fogos-Trees/kernel_motorola_sm6375.git kernel/motorola/sm6375
+
+rm -rf hardware/motorola
+git clone https://github.com/LineageOS/android_hardware_motorola.git -b lineage-21 hardware/motorola
+
+git clone https://github.com/Drpto/packages_apps_prebuilt-camera.git packages/apps/prebuilt
